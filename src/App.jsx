1f82f01a541b9f15286956a023dac1bf1bc24f6b
Hoang <phuvanhoang@gmail.com>
2024-11-02 21:06:00 +0700

    add something and delete a task

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,68 @@
 import Form from "./components/Form";
 import FilterButton from "./components/FilterButton";
 import Todo from "./components/Todo";
+import { useState } from "react";
+import { nanoid } from "nanoid";
 
 function App(props) {
-  let getTask = (task) => {
-    return (
-      <Todo
-        id={task.id}
-        name={task.name}
-        completed={task.completed}
-        key={task.id}
-      />
-    );
-  };
+  // keep these logs for remembering
+  console.log("Call App");
+  const [tasks, setTasks] = useState(props.tasks);
+  console.log(tasks);
 
-  const taskList = props.tasks?.map(getTask);
+  function addTask(name) {
+    console.log("Call addTask");
+    const newTask = { id: `todo-${nanoid()}`, name: name, completed: false };
+    setTasks([...tasks, newTask]);
+  }
+
+  function toggleTaskCompleted(id) {
+    console.log("Call toggleTaskCompleted");
+    const newTasks = tasks.map((task) => {
+      // if this task has the same ID as the edited task
+      if (task.id == id) {
+        // use object spread to make a new object
+        // whose `completed` prop has been inverted
+        // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/map#mapped_array_contains_undefined
+        // check Overriding properties if setting wrong order
+        return { ...task, completed: !task.completed };
+      }
+      return task;
+    });
+
+    setTasks(newTasks);
+  }
+
+  function deleteTask(id) {
+    console.log("Call deleteTask");
+    const remainingTasks = tasks.filter((task) => task.id !== id);
+    setTasks(remainingTasks);
+  }
+
+  const taskList = tasks?.map((task) => (
+    <Todo
+      id={task.id}
+      name={task.name}
+      completed={task.completed}
+      key={task.id}
+      toggleTaskCompleted={toggleTaskCompleted}
+      deleteTask={deleteTask}
+    />
+  ));
+
+  const taskNoun = taskList.length != 1 ? "tasks" : "task";
+  const headingText = `${taskList.length} ${taskNoun} remaining`;
 
   return (
     <div className="todoapp stack-large">
       <h1>TodoMatic</h1>
-      <Form />
+      <Form addTask={addTask} />
       <div className="filters btn-group stack-exception">
         <FilterButton />
         <FilterButton />
         <FilterButton />
       </div>
-      <h2 id="list-heading">3 tasks remaining</h2>
+      <h2 id="list-heading">{headingText}</h2>
       <ul
         role="list"
         className="todo-list stack-large stack-exception"
